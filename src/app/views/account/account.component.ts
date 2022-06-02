8d17f4c6f32e48888ebab2443efaecf53e45509a
emelinepal <emeline43@outlook.fr>
2022-06-02 16:41:18 +0200

    account fields

--- src/app/views/account/account.component.ts
+++ src/app/views/account/account.component.ts
@@ -1,16 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
 	selector: 'app-account',
 	templateUrl: './account.component.html',
-	styleUrls: ['./account.component.scss']
+	styleUrls: ['./account.component.scss'],
 })
 export class AccountComponent implements OnInit {
+	generalFormGrp: FormGroup = new FormGroup({
+		pseudo: new FormControl(),
+		email: new FormControl(),
+	});
 
-	constructor () {
+	changePasswordFormGrp: FormGroup = new FormGroup({
+		password: new FormControl(),
+		confirmPassword: new FormControl(),
+	});
+
+	constructor() {}
+
+	ngOnInit(): void {}
+
+	get generalFormPseudo() {
+		return this.generalFormGrp.get('pseudo') as FormControl;
 	}
-
-	ngOnInit (): void {
+	get generalFormEmail() {
+		return this.generalFormGrp.get('email') as FormControl;
 	}
-
+	get changePasswordFormPassword() {
+		return this.changePasswordFormGrp.get('password') as FormControl;
+	}
+	get changePasswordFormConfirmPassword() {
+		return this.changePasswordFormGrp.get('confirmPassword') as FormControl;
+	}
 }
