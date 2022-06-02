5a7bfe7a1b06f8239b47c60e2b584bba95b5fca3
arty3p <gsanchez.dev@gmail.com>
2022-06-02 11:32:54 +0200

    feat (routing) : setup game lobby component

--- src/app/views/games/games.module.ts
+++ src/app/views/games/games.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { GameCreateComponent } from './game-create/game-create.component';
+import { GameLobbyComponent } from './game-lobby/game-lobby.component';
 
 const routes: Routes = [
-	{ path: 'create', component: GameCreateComponent }
+	{ path: 'create', component: GameCreateComponent },
+	{ path: 'g/:gameId/lobby', component: GameCreateComponent }
 ]
 
 @NgModule({
 	declarations: [
-		GameCreateComponent
+		GameCreateComponent,
+		GameLobbyComponent
 	],
 	imports: [
 		CommonModule,
