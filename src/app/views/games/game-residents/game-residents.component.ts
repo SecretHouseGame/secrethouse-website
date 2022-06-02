7f94d9ac8cd73b9e2094bb17389694a8b581e4d9
arty3p <gsanchez.dev@gmail.com>
2022-06-02 11:34:46 +0200

    feat (routing) : setup game residents component

---
+++ src/app/views/games/game-residents/game-residents.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'app-game-residents',
+	templateUrl: './game-residents.component.html',
+	styleUrls: ['./game-residents.component.scss']
+})
+export class GameResidentsComponent implements OnInit {
+
+	constructor () {
+	}
+
+	ngOnInit (): void {
+	}
+
+}
--- src/app/views/games/games.module.ts
+++ src/app/views/games/games.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from "@angular/router";
 import { GameCreateComponent } from './game-create/game-create.component';
 import { GameLobbyComponent } from './game-lobby/game-lobby.component';
+import { GameResidentsComponent } from './game-residents/game-residents.component';
 
 const routes: Routes = [
-	{ path: 'create', component: GameCreateComponent },
-	{ path: 'g/:gameId/lobby', component: GameCreateComponent }
+	{path: 'create', component: GameCreateComponent},
+	{path: 'g/:gameId/lobby', component: GameCreateComponent},
+	{path: 'g/:gameId/residents', component: GameCreateComponent},
 ]
 
 @NgModule({
 	declarations: [
 		GameCreateComponent,
-		GameLobbyComponent
+		GameLobbyComponent,
+		GameResidentsComponent
 	],
 	imports: [
 		CommonModule,
