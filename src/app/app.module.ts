69ab3698a7d06889e791ad82c6d85db705b00b1a
Aimee <aimeeritleng@outlook.com>
2022-06-03 09:03:52 +0200

    feat: add footer (need style)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgxDsSecretHouseModule } from 'ngx-ds-secret-house';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
+  		FooterComponent,
 	],
 	imports: [
 		BrowserModule,
