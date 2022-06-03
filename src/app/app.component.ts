d0e26d122a4ac25a6847439b773cfbe8e66dbb85
Aimee <aimeeritleng@outlook.com>
2022-06-03 11:58:51 +0200

    feat: add basic header & footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, Event as NavigationEvent, NavigationStart } from "@angular/router";
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'secrethouse-website';
+	title : string = 'secrethouse-website';
+	displayNav : boolean = true;
+	currentRoute : string = '';
+
+	constructor(private router: Router) {
+		this.router.events
+		.subscribe(
+		  (event: NavigationEvent) => {
+			if(event instanceof NavigationStart) {
+			  if(event.url == "/") this.displayNav = false;
+			}
+		});
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgxDsSecretHouseModule } from 'ngx-ds-secret-house';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './layout/nav/nav.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
 
 @NgModule({
 	declarations: [
 		AppComponent,
+		NavComponent,
   		FooterComponent,
+    NavComponent,
 	],
 	imports: [
 		BrowserModule,
