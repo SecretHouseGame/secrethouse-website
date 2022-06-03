de7fdf0c9251f8242d97f193980dde85a327072c
arty3p <gsanchez.dev@gmail.com>
2022-06-03 17:07:52 +0200

    feat (dashboard) : in progress

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
 
 @Component({
 	selector: 'app-dashboard',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+	gameCode: FormControl = new FormControl('')
 
 	constructor () {
 	}
