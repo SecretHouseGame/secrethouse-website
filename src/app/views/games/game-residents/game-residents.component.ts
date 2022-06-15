f7d382cf0bc543ddd891e7da84c53092027efa7b
arty3p <gsanchez.dev@gmail.com>
2022-06-15 23:40:28 +0200

    feat (residents) : card and resident informations

--- src/app/views/games/game-residents/game-residents.component.ts
+++ src/app/views/games/game-residents/game-residents.component.ts
@@ -10,10 +10,42 @@ import { listItem } from 'ngx-ds-secret-house/lib/components/interfaces/list-ite
 })
 export class GameResidentsComponent implements OnInit {
 	residents: Player[] = [
-		{ id: 1, genre: 'female', cagnotte: 1500, name: 'Catherine', isEliminated: false, secret: '' },
-		{ id: 2, genre: 'male', cagnotte: 900, name: 'Bryan', isEliminated: true, secret: 'Il a mangé son frère siamois' },
-		{ id: 3, genre: 'female', cagnotte: 12200, name: 'Julie', isEliminated: false, secret: '' },
-		{ id: 4, genre: 'male', cagnotte: 4560, name: 'Nathan', isEliminated: false, secret: '' },
+		{
+			id: 1,
+			genre: 'female',
+			cagnotte: 1500,
+			name: 'Catherine',
+			isEliminated: false,
+			secret: '',
+			avatar: '/assets/images/players/catherine.png'
+		},
+		{
+			id: 2,
+			genre: 'male',
+			cagnotte: 900,
+			name: 'Bryan',
+			isEliminated: true,
+			secret: 'Il a mangé son frère siamois',
+			avatar: '/assets/images/players/bryan.png'
+		},
+		{
+			id: 3,
+			genre: 'female',
+			cagnotte: 12200,
+			name: 'Julie',
+			isEliminated: false,
+			secret: '',
+			avatar: '/assets/images/players/julie.png'
+		},
+		{
+			id: 4,
+			genre: 'male',
+			cagnotte: 4560,
+			name: 'Nathan',
+			isEliminated: false,
+			secret: '',
+			avatar: '/assets/images/players/nathan.png'
+		},
 	]
 	selectedResident: Player = {}
 
@@ -23,14 +55,22 @@ export class GameResidentsComponent implements OnInit {
 	}
 
 	ngOnInit (): void {
+		// TODO : utiliser aussi le model user pour récupérer les autres informations
 		this.selectedResident = this.residents[0]
 
 		this.residents.forEach((resident) => {
 			this.items.push(<listItem>{
-				id: resident.id,
-				text: resident.name
+				text: resident.name,
+				img: {
+					src: resident.avatar,
+					alt: 'img'
+				}
 			})
 		})
+	}
+
+	getResidentGenderTranslation (gender: string) {
+		return gender === 'male' ? 'Homme' : 'Femme'
 	}
 
 	selectResident (residentId: number) {
