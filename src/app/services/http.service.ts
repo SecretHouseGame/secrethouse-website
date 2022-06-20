4ca6c9b39f42048c8bcaf03ef9ccbca549bedd13
Fayaah <louise.baulan@gmail.com>
2022-06-20 11:50:45 +0200

    feat(character) : creation of page edit my player

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -13,9 +13,18 @@ export class HttpService {
 	constructor (public httpClient: HttpClient) {
 	}
 
+	/** Récupérer le personnage du joueur **/
+	public getCurrentPlayer(): Observable<Player> {
+		return this.httpClient.get<Player>(`${this.fakeDbUrl}/currentPlayer`)
+	}
+	public updateCurrentPlayer(body: object): Observable<Player> {
+		return this.httpClient.put<Player>(`${this.fakeDbUrl}/currentPlayer`, body);
+	}
+
 	/** Récupérer tous les habitants */
 	public getPlayers(): Observable<Player[]> {
 		return this.httpClient.get<Player[]>(`${this.fakeDbUrl}/players`);
 	}
+	// todo : update players when updated currentPlayer
 
 }
---
+++ src/app/views/games/game-character/game-character.component.ts
@@ -0,0 +1,90 @@
+import {Component, Input, OnInit, ViewEncapsulation} from "@angular/core";
+import {Player} from "../../../interfaces/player";
+import {FormControl, FormGroup} from "@angular/forms";
+import {SelectOption} from "ngx-ds-secret-house/lib/components/interfaces/select-option";
+import {HttpService} from "../../../services/http.service";
+
+@Component({
+	selector: 'app-game-character',
+	templateUrl: './game-character.component.html',
+	styleUrls: ['./game-character.component.scss'],
+	encapsulation: ViewEncapsulation.None
+})
+// fixme : missing eye icon in bg
+export class GameCharacterComponent implements OnInit {
+	// fixme : validate genders
+	genders: SelectOption[] = [
+		{id: "0", name: "Homme", value: "MALE"},
+		{id: "1", name: "Femme", value: "FEMALE"},
+		{id: "2", name: "Autre", value: "OTHER"}
+	]
+
+	// fixme : missing age & bio in interface player
+	character: Player = {
+		id: 0,
+	};
+
+	formGroup: FormGroup = new FormGroup({
+		name: new FormControl(),
+		age: new FormControl(),
+		genre: new FormControl(),
+		secret: new FormControl(),
+		// fixme : how to upload file ?
+		avatar: new FormControl(),
+		bio: new FormControl(),
+	})
+
+	constructor(public httpService: HttpService) {
+
+	}
+
+	ngOnInit(): void {
+		this.httpService.getCurrentPlayer().subscribe((player) => {
+			this.character = player
+
+			this.formGroup.setValue({
+				name: this.character.name ? this.character.name : "Name",
+				age: this.character.age ? this.character.age : "Age",
+				genre: this.character.genre ? this.character.genre : "Genre",
+				secret: this.character.secret ? this.character.secret : "Secret",
+				avatar: this.character.avatar ? this.character.avatar : "/assets/images/players/catherine.png",
+				bio: this.character.bio ? this.character.bio : "Bio"
+			})
+		})
+
+	}
+
+	// todo : missing avatar
+	get formGroupName() {
+		return this.formGroup.get('name') as FormControl;
+	}
+
+	get formGroupAge() {
+		return this.formGroup.get('age') as FormControl;
+	}
+
+	get formGroupGenre() {
+		return this.formGroup.get('genre') as FormControl;
+	}
+
+	get formGroupSecret() {
+		return this.formGroup.get('secret') as FormControl;
+	}
+
+	get formGroupBio() {
+		return this.formGroup.get('bio') as FormControl;
+	}
+
+	updateCharacter(): void {
+		let updatedCurrentPlayer = this.character;
+		updatedCurrentPlayer.name = this.formGroupName.value;
+		updatedCurrentPlayer.age = this.formGroupAge.value;
+		updatedCurrentPlayer.genre = this.formGroupGenre.value;
+		updatedCurrentPlayer.secret = this.formGroupSecret.value;
+		updatedCurrentPlayer.bio = this.formGroupBio.value;
+		updatedCurrentPlayer.name = this.formGroupName.value;
+
+		this.httpService.updateCurrentPlayer(updatedCurrentPlayer);
+		this.character = updatedCurrentPlayer;
+	}
+}
